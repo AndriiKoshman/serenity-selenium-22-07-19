51aaf34a4c8c57a53cd917d1275595ddfd5fa53c
Andrii Koshman <andriikoshman@gmail.com>
2019-08-01 13:30:49 +0300

    lesson#2

--- src/test/java/test/LinkedinLoginTest.java
+++ src/test/java/test/LinkedinLoginTest.java
@@ -3,9 +3,6 @@ package test;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static net.thucydides.core.matchers.BeanMatchers.the;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SerenityRunner.class)
 public class LinkedinLoginTest extends BaseTest {
@@ -14,6 +11,6 @@ public class LinkedinLoginTest extends BaseTest {
     public void successfulLoginTest() {
         user.openLandingPage();
         user.logIn("mineqabox@gmail.com","qualityassurance1");
-        user.shouldBeLoggedIn(the("titles", containsString("LinkedIn")));
+        user.shouldBeLoggedIn();
     }
 }
